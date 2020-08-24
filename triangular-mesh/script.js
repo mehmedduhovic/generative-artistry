b7b070414539c03b15b7479791d8355a4dfbacff
bracikaa <mehmed_duhovic@hotmail.com>
2020-08-25 01:34:12 +0200

    triangular mesh

--- triangular-mesh/script.js
+++ triangular-mesh/script.js
@@ -1,7 +1,7 @@
 let canvas = document.querySelector("canvas");
 let context = canvas.getContext("2d");
 
-let size = 400;
+let size = 500;
 let dpr = window.devicePixelRatio;
 canvas.width = size * dpr;
 canvas.height = size * dpr;
@@ -13,16 +13,16 @@ let line,
   dot,
   odd = false,
   lines = [],
-  gap = size / 8;
+  gap = size / 10;
 
-for (let y = gap / 2; y <= size; y += gap) {
+for (let y = gap / 2 + 50; y <= size - 50; y += gap) {
   odd = !odd;
   line = [];
-  for (let x = gap / 4; x <= size; x += gap) {
-    dot = { x: x + (odd ? gap / 2 : 0), y: y };
-    line.push(dot);
-    context.beginPath();
-    context.arc(dot.x, dot.y, 1, 0, 2 * Math.PI, true);
+  for (let x = gap / 4 + 50; x <= size - 50; x += gap) {
+    line.push({
+      x: x + (Math.random() * 0.8 - 0.2) * gap + (odd ? gap / 2 : 0),
+      y: y + (Math.random() * 0.8 - 0.2) * gap,
+    });
     context.fill();
   }
 
@@ -30,24 +30,32 @@ for (let y = gap / 2; y <= size; y += gap) {
 }
 
 function drawTriangle(pointA, pointB, pointC) {
-    context.beginPath();
-    context.moveTo(pointA.x, pointA.y);
-    context.lineTo(pointB.x, pointB.y);
-    context.lineTo(pointC.x, pointC.y);
-    context.lineTo(pointA.x, pointA.y);
-    context.closePath;
-    context.stroke();
+  context.beginPath();
+  context.moveTo(pointA.x, pointA.y);
+  context.lineTo(pointB.x, pointB.y);
+  context.lineTo(pointC.x, pointC.y);
+  context.lineTo(pointA.x, pointA.y);
+  context.closePath();
+  h = 240;
+  s = Math.floor(Math.random() * 100);
+  l = Math.floor(Math.random() * 100);
+  context.fillStyle = "hsl(" + h + ", " + s + "%, " + l + "%)";
+  context.fill();
+  context.stroke();
 }
 
 let dotline;
 odd = true;
 
-for(let y = 0; y < lines.length - 1; y++) {
-    odd = !odd;
-    dotline = [];
-    for(let i = 0; i < lines[y].length; i++) {
-        dotline.push(odd ? lines[y][i] : lines[y + 1][i]);
-        dotline.push(odd ? lines[y + 1][i] : lines[y][i]);
-    }
+for (let y = 0; y < lines.length - 1; y++) {
+  odd = !odd;
+  dotline = [];
+  for (let i = 0; i < lines[y].length; i++) {
+    dotline.push(odd ? lines[y][i] : lines[y + 1][i]);
+    dotline.push(odd ? lines[y + 1][i] : lines[y][i]);
+  }
 
+  for (let i = 0; i < dotline.length - 2; i++) {
+    drawTriangle(dotline[i], dotline[i + 1], dotline[i + 2]);
+  }
 }
