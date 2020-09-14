aa2f8260a525efbabb290b87f0cee39a5924fc4c
bracikaa <mehmed_duhovic@hotmail.com>
2020-09-14 14:16:12 +0200

    circle packing done

--- circle-packing/script.js
+++ circle-packing/script.js
@@ -1,7 +1,6 @@
 let canvas = document.querySelector("canvas");
 let context = canvas.getContext("2d");
 
-
 let size = 400;
 let dpr = window.devicePixelRatio;
 canvas.width = size * dpr;
@@ -16,15 +15,31 @@ let totalCircles = 400;
 let createCircleAttempt = 400;
 
 function createAndDrawCircle() {
-  let newCircle = {
-    x: Math.floor(Math.random() * size),
-    y: Math.floor(Math.random() * size),
-    radius: minRadius
-  };
+  let newCircle;
+  let circleSafeToDraw = false;
 
-  for(let radiusSize = minRadius; radiusSize < maxRadius; radiusSize++){
+  for (let tries = 0; tries < createCircleAttempt; tries++) {
+    newCircle = {
+      x: Math.floor(Math.random() * size),
+      y: Math.floor(Math.random() * size),
+      radius: minRadius,
+    };
+
+    if (doesCircleHaveACollision(newCircle)) {
+      continue;
+    } else {
+      circleSafeToDraw = true;
+      break;
+    }
+  }
+
+  if (!circleSafeToDraw) {
+    return;
+  }
+
+  for (let radiusSize = minRadius; radiusSize < maxRadius; radiusSize++) {
     newCircle.radius = radiusSize;
-    if(doesCircleHaveACollision(newCircle)) {
+    if (doesCircleHaveACollision(newCircle)) {
       newCircle.radius--;
       break;
     }
@@ -32,24 +47,41 @@ function createAndDrawCircle() {
 
   circles.push(newCircle);
   context.beginPath();
-  context.arc(newCircle.x, newCircle.y, newCircle.radius, 0, 2*Math.PI);
+  context.arc(newCircle.x, newCircle.y, newCircle.radius, 0, 2 * Math.PI);
   context.stroke();
 
   function doesCircleHaveACollision(circle) {
-    for(let i = 0; i < circles.length; i++) {
+    for (let i = 0; i < circles.length; i++) {
       let otherCircle = circles[i];
       let combinedRadius = circle.radius + otherCircle.radius;
       let xCoord = circle.x - otherCircle.x;
       let yCoord = circle.y - otherCircle.y;
 
-      if(combinedRadius >= Math.sqrt((xCoord * xCoord) + (yCoord * yCoord))) {
+      if (combinedRadius >= Math.sqrt(xCoord * xCoord + yCoord * yCoord)) {
         return true;
       }
     }
+
+    if (circle.x + circle.radius >= size || circle.x - circle.radius <= 0) {
+      return true;
+    }
+
+    if (circle.y + circle.radius >= size || circle.y - circle.radius <= 0) {
+      return true;
+    }
+
+    if (circle.x + circle.radius >= size || circle.x - circle.radius <= 0) {
+      return true;
+    }
+
+    if (circle.y + circle.radius >= size || circle.y - circle.radius <= 0) {
+      return true;
+    }
+
     return false;
   }
 }
 
-for(let i = 0; i < totalCircles; i++) {
+for (let i = 0; i < totalCircles; i++) {
   createAndDrawCircle();
 }
